25856d04b686b039cb08e7242ef725531f6f1976
satyamgkg <97548391+satyamgkg@users.noreply.github.com>
2023-01-30 17:23:43 +0530

    Add files via upload

---
+++ Collection.java
@@ -0,0 +1,19 @@
+// Collections framework
+
+
+import java.util.ArrayList;
+
+class Collections
+{
+    public static void main(String[] args)
+{
+        ArrayList<String> animals = new ArrayList<>();
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Horse");
+        animals.add("Lion");
+        animals.add("Elephant");
+
+        System.out.println("ArrayList: " + animals);
+    }
+}
---
+++ Collection2.java
@@ -0,0 +1,20 @@
+// Collection linkedlist
+
+import java.util.LinkedList;
+
+class Collection2 
+{
+  public static void main(String[] args){
+
+    
+    LinkedList<String> l1 = new LinkedList<>();
+
+    l1.add("1003");
+    l1.add("1004");
+    l1.add("1005");
+    l1.add("1006");
+    l1.add("1007");
+
+    System.out.println(l1);
+  }
+}
